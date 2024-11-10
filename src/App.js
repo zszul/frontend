764dff1e9620a2520b5f4af1f9bb27a9d97571b7
zszul <150448992+zszul@users.noreply.github.com>
2024-11-10 13:30:06 -0600

    Basic fetching from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const getDrugInfo = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3001/api/drug/aspirin"
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching drug data:", error);
+      }
+    };
+
+    getDrugInfo();
+  }, []);
+
+  return <div>Check the console for drug info.</div>;
 }
 
 export default App;
